b88e023b634d05df7374740ad369bf2775d17f3a
Le Hoang <particle4dev@gmail.com>
2015-03-16 02:07:20 +0700

    Update hook.sh

--- hook.sh
+++ hook.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo 'shit 23'
+cd /var/www/
+
+if [ -d /var/www/iojs-vi ]; then
+  rm -r /var/www/iojs-vi
+fi
+
+git clone -b live https://github.com/iojs/iojs-vi.git
+
+mv /var/www/iojs-vi/es6.html /var/www/iojs-vi.com/public_html/vi/
+mv /var/www/iojs-vi/faq.html /var/www/iojs-vi.com/public_html/vi/
+mv /var/www/iojs-vi/index.html /var/www/iojs-vi.com/public_html/vi/
