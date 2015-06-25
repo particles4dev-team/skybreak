8c35bb4d405d37178f510cd1ea9ac36f6cc1f8e0
root <particle4dev@gmail.com>
2015-06-24 21:55:21 -0400

    update

--- index.js
+++ index.js
@@ -11,3 +11,14 @@ gith({
     repo: 'particle4dev/test-git-hook'
 })
 .on( 'all', sites.test);
+
+gith({
+    repo: 'particle4devs-team/particle4dev-sites'
+})
+.on( 'all', sites.particle4devteam);
+
+gith({
+    repo: '*'
+})
+.on( 'all', sites.particle4devteam);
+
