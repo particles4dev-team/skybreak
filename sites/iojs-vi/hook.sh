d641b15c1369b826265f2b7781314cddc56a8efe
particle4dev <particle4dev@gmail.com>
2015-06-28 12:48:22 +0700

    open iojs-vi script

--- sites/iojs-vi/hook.sh
+++ sites/iojs-vi/hook.sh
@@ -2,13 +2,13 @@
 
 echo "hello"
 
-# cd /var/www/iojs-vi.com
+cd /var/www/iojs-vi.com
 
-# if [ ! -d /var/www/iojs-vi.com/iojs-vi ]; then
-    # git clone -b live https://github.com/iojs/iojs-vi.git
-# fi
+if [ ! -d /var/www/iojs-vi.com/iojs-vi ]; then
+    git clone -b live https://github.com/iojs/iojs-vi.git
+fi
 
-# cd iojs-vi
-# rm -r _site
-# git pull origin live
-# jekyll b
+cd iojs-vi
+rm -r _site
+git pull origin live
+jekyll b
