dd6bb46fee2bf6bed142e00e61363185d1f36e4b
particle4dev <particle4dev@gmail.com>
2015-07-13 10:39:21 +0700

    add gulp

---
+++ gulp/tasks/clean.js
@@ -0,0 +1,12 @@
+/**
+ * Stability: 1 - Experimental
+ */
+
+var gulp    = require('gulp');
+var del     = require('del');
+var config  = require('../config');
+
+gulp.task('clean', function(cb) {
+    // clean out directory before build
+    del(config.del.files, cb);
+});
